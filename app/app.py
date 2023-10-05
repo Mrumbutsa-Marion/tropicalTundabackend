7f827508f9c0d8fc582b3c6c4f32f55b5f87cf0e
Mrumbutsa-Marion <marion.mwanje@student.moringaschool.com>
2023-10-05 08:34:40 +0300

    finished working with the authentication

--- app/app.py
+++ app/app.py
@@ -1,11 +1,71 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_migrate import Migrate
+from models import db, User
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email
+import secrets
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tunda.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+secret_key = secrets.token_hex(16)
+app.config['SECRET_KEY'] = secret_key
+migrate = Migrate(app, db)
+db.init_app(app)
+
+# Define a WTForms class for user signup
+class SignupForm(FlaskForm):
+    user_name = StringField('user_name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+
+# Define a WTForms class for user login
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+    return "Welcome to my app"
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    data = request.json
+
+    user_name = data.get('user_name')
+    email = data.get('email')
+    password = data.get('password')
+
+    existing_user = User.query.filter_by(email=email).first()
+    if existing_user:
+        return jsonify({'message': 'User with this email already exists'}), 409
+
+    hashed_password = generate_password_hash(password)
+
+    new_user = User(user_name=user_name, email=email, password=hashed_password)
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({'message': 'User created successfully'})
+
+# Create the /login route to handle user login
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+
+    email = data.get('email')
+    password = data.get('password')
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        return jsonify({'message': 'Invalid credentials'}), 401
+
+    return jsonify({'message': 'Login successful'})
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    app.run()
---
+++ app/models.py
@@ -0,0 +1,55 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import validates
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(255), nullable=False)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    orders = db.relationship('Order', backref='user', lazy=True)
+    donations = db.relationship('Donation', backref='user', lazy=True)
+    cart_items = db.relationship('CartItem', backref='user', lazy=True)
+    
+    def __init__(self, user_name, email, password):
+        self.user_name = user_name
+        self.email = email
+        self.password = password
+
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    items = db.Column(db.String)
+    total_amount = db.Column(db.Float)
+    delivery_address = db.Column(db.String)
+
+class Donation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    donation_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Float)
+    timestamp = db.Column(db.DateTime)
+    message = db.Column(db.String)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    description = db.Column(db.String)
+    price = db.Column(db.Float)
+    quantity = db.Column(db.Integer)
+
+class CartItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer)
+
+class Payment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    amount = db.Column(db.Integer)
+    date = db.Column(db.DateTime)
